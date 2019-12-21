d7a0a0d75f0bd5629dc2545a97b0479402ba43ab
Ant <anthony.wong3485@gmail.com>
2019-12-21 08:39:51 -0800

    Initial Commit

---
+++ U_Owe_MeTests/U_Owe_MeTests.swift
@@ -0,0 +1,34 @@
+//
+//  U_Owe_MeTests.swift
+//  U_Owe_MeTests
+//
+//  Created by Anthony on 2019-12-21.
+//  Copyright © 2019 Anthony. All rights reserved.
+//
+
+import XCTest
+@testable import U_Owe_Me
+
+class U_Owe_MeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
